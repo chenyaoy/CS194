e6addda3ee3b41659379148934a76e2e8c92d09f
William Huang <whuang6@stanford.edu>
2017-02-04 15:56:04 -0800

    fixing index.js

--- Server/index.js
+++ Server/index.js
@@ -12,11 +12,11 @@ if (!databaseUri) {
 }
 
 var api = new ParseServer({
-  databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
+  databaseURI: databaseUri || 'mongodb://heroku_p1mrzl77:t7e26ldoehk6gp2qakav4e88qe@ds139979.mlab.com:39979/heroku_p1mrzl77',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-  appId: process.env.APP_ID || 'myAppId',
-  masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
-  serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
+  appId: process.env.APP_ID || 'codeconomy',
+  masterKey: process.env.MASTER_KEY || 'flatstanley', //Add your master key here. Keep it secret!
+  serverURL: process.env.SERVER_URL || 'http://codeconomy.herokuapp.com/parse',  // Don't forget to change to https if needed
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
   }
