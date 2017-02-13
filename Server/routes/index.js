4df5a6a42e7c86ba409d9a1263ee981e198c7da4
hapoore <hapoore@stanford.edu>
2017-02-12 16:59:43 -0800

    experimenting

--- Server/routes/index.js
+++ Server/routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res) {
-    res.send(__dirname);
+    res.send(path.join(__dirname, '../views/hello_world.html')));
 });
 
 
