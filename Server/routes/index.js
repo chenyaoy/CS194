307a26aa5079836c080538050ba5777824c9062c
hapoore <hapoore@stanford.edu>
2017-02-12 16:49:26 -0800

    experimenting

--- Server/routes/index.js
+++ Server/routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res) {
-    res.render('hello_world.html');
+    res.send('index home page');
 });
 
 
