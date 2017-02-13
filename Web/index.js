82fb8aa8bfa47c3c8ce2945ff4ac583887efe5c2
hapoore <hapoore@stanford.edu>
2017-02-13 11:55:04 -0800

    Started building page header with bootstrap

--- Web/index.js
+++ Web/index.js
@@ -14,8 +14,8 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.get('/', function(request, response) {
-  response.render('pages/index');
+app.get('/', function(req, res) {
+  res.render('pages/index', {balance:50});
 });
 
 app.listen(app.get('port'), function() {
--- Web/routes/coupons.js
+++ Web/routes/coupons.js
@@ -3,7 +3,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res) {
-    res.render('pages/index');
+    res.render('pages/index',{balance:50});
 });
 
 router.post('/', function(req, res) {
