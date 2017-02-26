35b462cb2a08c62b6adecd9c6762be9dfc797e32
Chenyao <icyu1027@gmail.com>
2017-02-26 02:07:06 -0800

    Added rudimentary user signups and login

--- Web/routes/coupons.js
+++ Web/routes/coupons.js
@@ -10,12 +10,13 @@ router.use(bodyParser.json()); // support json encoded bodies
 router.use(bodyParser.urlencoded({ extended: true }));
 
 router.post('/postCoupon/submit', function(req, res) {
+    var user = Parse.User.current();
     if(validateRequiredCouponParams(req)) {
         var Coupon = Parse.Object.extend("Coupon");
         var coupon = new Coupon();
         coupon.set("storeName", req.body.store);
         coupon.set("description", req.body.couponDescription);
-        coupon.set("expirationDate", 
+        coupon.set("expirationDate",
             req.body.expireDate.length == 0 ? null : new Date(req.body.expireDate));
         coupon.set("additionalInfo", req.body.additionalInfo);
         coupon.set("price", req.body.price);
@@ -23,9 +24,17 @@ router.post('/postCoupon/submit', function(req, res) {
         coupon.set("category", req.body.category);
         coupon.set("status", "unsold");
         coupon.set("deleted", false);
+        coupon.set("seller", user);
         coupon.save(null, {
             success: function(coupon) {
-                res.send("Coupon uploaded successfully.");
+                var query = new Parse.Query(Coupon);
+                query.equalTo("seller", user);
+                query.find({
+                    success: function(userCoupons) {
+                        res.send(userCoupons);
+                    }
+                });
+                // res.send("Coupon uploaded successfully.");
             },
             error: function(coupon, error) {
                 res.send("Coupon upload failed. Please try again. " + error.message)
--- Web/routes/users.js
+++ Web/routes/users.js
@@ -1,6 +1,13 @@
 var express = require('express');
+var Parse = require('parse/node');
+Parse.initialize("codeconomy");
+Parse.serverURL = 'http://codeconomy.herokuapp.com/parse';
+
+var bodyParser = require('body-parser');
 
 var router = express.Router();
+router.use(bodyParser.json()); // support json encoded bodies
+router.use(bodyParser.urlencoded({ extended: true }));
 
 router.get('/', function(req, res) {
     res.render('pages/index');
@@ -8,6 +15,52 @@ router.get('/', function(req, res) {
 
 router.post('/', function(req, res) {
     res.send('POST handler for /users route.');
+});
+
+router.get('/login', function(req, res) {
+    res.render('pages/users/login',{balance:50});
+});
+
+router.post('/login/submit', function(req, res) {
+    Parse.User.logIn(req.body.username, req.body.password, {
+      success: function(user) {
+        // Do stuff after successful login.
+        res.send("Successfully logged in");
+      },
+      error: function(user, error) {
+        // The login failed. Check error to see why.
+        res.send("Error: " + error.code + " " + error.message);
+      }
+    });
+});
+
+router.get('/signup', function(req, res) {
+    res.render('pages/users/signup',{balance:50});
+});
+
+router.post('/signup/submit', function(req, res) {
+    var user = new Parse.User();
+    user.set("username", req.body.username);
+    user.set("password", req.body.password);
+    user.set("email", req.body.email);
+
+    user.signUp(null, {
+      success: function(user) {
+        // Hooray! Let them use the app now.
+        res.send("User signed up successfully.");
+      },
+      error: function(user, error) {
+        // Show the error message somewhere and let the user try again.
+        res.send("Error: " + error.code + " " + error.message);
+      }
+    });
+});
+
+
+router.post('/logout', function(res, res) {
+    Parse.User.logOut().then(() => {
+        var currentUser = Parse.User.current();  // this will now be null
+    });
 });
 
 module.exports = router;
