4cf95ef72a397f3e4ab47e1cf568fe5cd9ff69e4
Gary Thung <garythung@gmail.com>
2017-03-12 16:50:27 -0700

    Make background proper gray on load.

---
+++ iPhoneApp/Codeconomy/Codeconomy/KeysViewController.h
@@ -0,0 +1,13 @@
+//
+//  KeysViewController.h
+//  Codeconomy
+//
+//  Created by Gary on 03/12/17.
+//  Copyright © 2017 Stanford. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KeysViewController : UIViewController
+
+@end
