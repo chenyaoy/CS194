e300a86fad58fd3b42cb5b0e7f6046e5eb9e0254
Gary Thung <garythung@gmail.com>
2017-03-15 19:19:02 -0700

    Added card entry to options view. need to do some textfield controls and add buy button.

--- iPhoneApp/Codeconomy/Codeconomy/KeysBuyingOptionsView.m
+++ iPhoneApp/Codeconomy/Codeconomy/KeysBuyingOptionsView.m
@@ -17,6 +17,16 @@
 @property (nonatomic, strong) UIButton *buyTenKeys;
 @property (nonatomic, strong) UIButton *buyFiftyKeys;
 @property (nonatomic, strong) UIButton *buyHundredKeys;
+@property (nonatomic, strong) UIButton *selectedKeys;
+
+@property (nonatomic, strong) UILabel *cardNumberLabel;
+@property (nonatomic, strong) UILabel *expirationDateLabel;
+@property (nonatomic, strong) UILabel *securityCodeLabel;
+@property (nonatomic, strong) UILabel *zipCodeLabel;
+@property (nonatomic, strong) UITextField *cardNumberField;
+@property (nonatomic, strong) UITextField *expirationDateField;
+@property (nonatomic, strong) UITextField *securityCodeField;
+@property (nonatomic, strong) UITextField *zipCodeField;
 @end
 
 @implementation KeysBuyingOptionsView
@@ -85,6 +95,70 @@
         _buyHundredKeys.layer.opacity = 0.33;
         [self addSubview:_buyHundredKeys];
         
+        _cardNumberLabel = [[UILabel alloc] init];
+        _cardNumberLabel.text = @"Card Number";
+        _cardNumberLabel.font = [UIFont systemFontOfSize:16.0];
+        [self addSubview:_cardNumberLabel];
+        
+        _expirationDateLabel = [[UILabel alloc] init];
+        _expirationDateLabel.text = @"Expiration Date";
+        _expirationDateLabel.font = [UIFont systemFontOfSize:16.0];
+        [self addSubview:_expirationDateLabel];
+        
+        _securityCodeLabel = [[UILabel alloc] init];
+        _securityCodeLabel.text = @"Security Code";
+        _securityCodeLabel.font = [UIFont systemFontOfSize:16.0];
+        [self addSubview:_securityCodeLabel];
+        
+        _zipCodeLabel = [[UILabel alloc] init];
+        _zipCodeLabel.text = @"Zip Code";
+        _zipCodeLabel.font = [UIFont systemFontOfSize:16.0];
+        [self addSubview:_zipCodeLabel];
+        
+        _cardNumberField = [[UITextField alloc] init];
+        _cardNumberField.placeholder = @"Add Card Number";
+        _cardNumberField.textAlignment = NSTextAlignmentLeft;
+        _cardNumberField.layer.cornerRadius = 10;
+        _cardNumberField.layer.masksToBounds = YES;
+        _cardNumberField.layer.borderWidth = 1.0f;
+        _cardNumberField.layer.borderColor = [[UIColor blackColor] CGColor];
+        _cardNumberField.backgroundColor = [UIColor clearColor];
+        _cardNumberField.layer.sublayerTransform = CATransform3DMakeTranslation(12.0, 0, 0);
+        [self addSubview:_cardNumberField];
+        
+        _expirationDateField = [[UITextField alloc] init];
+        _expirationDateField.placeholder = @"MM / YY";
+        _expirationDateField.textAlignment = NSTextAlignmentLeft;
+        _expirationDateField.layer.cornerRadius = 10;
+        _expirationDateField.layer.masksToBounds = YES;
+        _expirationDateField.layer.borderWidth = 1.0f;
+        _expirationDateField.layer.borderColor = [[UIColor blackColor] CGColor];
+        _expirationDateField.backgroundColor = [UIColor clearColor];
+        _expirationDateField.layer.sublayerTransform = CATransform3DMakeTranslation(12.0, 0, 0);
+        [self addSubview:_expirationDateField];
+        
+        _securityCodeField = [[UITextField alloc] init];
+        _securityCodeField.placeholder = @"CVV";
+        _securityCodeField.textAlignment = NSTextAlignmentLeft;
+        _securityCodeField.layer.cornerRadius = 10;
+        _securityCodeField.layer.masksToBounds = YES;
+        _securityCodeField.layer.borderWidth = 1.0f;
+        _securityCodeField.layer.borderColor = [[UIColor blackColor] CGColor];
+        _securityCodeField.backgroundColor = [UIColor clearColor];
+        _securityCodeField.layer.sublayerTransform = CATransform3DMakeTranslation(12.0, 0, 0);
+        [self addSubview:_securityCodeField];
+        
+        _zipCodeField = [[UITextField alloc] init];
+        _zipCodeField.placeholder = @"Zip Code";
+        _zipCodeField.textAlignment = NSTextAlignmentLeft;
+        _zipCodeField.layer.cornerRadius = 10;
+        _zipCodeField.layer.masksToBounds = YES;
+        _zipCodeField.layer.borderWidth = 1.0f;
+        _zipCodeField.layer.borderColor = [[UIColor blackColor] CGColor];
+        _zipCodeField.backgroundColor = [UIColor clearColor];
+        _zipCodeField.layer.sublayerTransform = CATransform3DMakeTranslation(12.0, 0, 0);
+        [self addSubview:_zipCodeField];
+        
         self.layer.cornerRadius = 10;
         self.layer.masksToBounds = YES;
     }
@@ -99,6 +173,15 @@
     [self.buyTenKeys sizeToFit];
     [self.buyFiftyKeys sizeToFit];
     [self.buyHundredKeys sizeToFit];
+    
+    [self.cardNumberField sizeToFit];
+    [self.expirationDateField sizeToFit];
+    [self.securityCodeField sizeToFit];
+    [self.zipCodeField sizeToFit];
+    [self.cardNumberLabel sizeToFit];
+    [self.expirationDateLabel sizeToFit];
+    [self.securityCodeLabel sizeToFit];
+    [self.zipCodeLabel sizeToFit];
     self.title.frame = CGRectMake((self.frame.size.width - self.title.frame.size.width) / 2.0,
                                  12.0,
                                  self.title.frame.size.width,
@@ -108,7 +191,7 @@
                                                           options:NSStringDrawingUsesLineFragmentOrigin
                                                        attributes:@{NSFontAttributeName:self.tenKeysQuantity.font}
                                                           context:nil].size;
-    self.buyTenKeys.frame = CGRectMake(self.frame.size.width - 20.0 - self.buyTenKeys.frame.size.width,
+    self.buyTenKeys.frame = CGRectMake(self.frame.size.width - 20.0 - 70.0,
                                       self.title.frame.origin.y + self.title.frame.size.height + 12.0,
                                       70.0,
                                       40.0);
@@ -142,15 +225,93 @@
                                             self.buyHundredKeys.frame.origin.y + (self.buyHundredKeys.frame.size.height - self.hundredKeysQuantity.frame.size.height) / 2,
                                             self.hundredKeysQuantity.frame.size.width,
                                             textSize.height);
+    
+    textSize = [self.cardNumberLabel.text boundingRectWithSize:CGSizeMake(self.cardNumberLabel.frame.size.width, MAXFLOAT)
+                                                           options:NSStringDrawingUsesLineFragmentOrigin
+                                                        attributes:@{NSFontAttributeName:self.cardNumberLabel.font}
+                                                           context:nil].size;
+    self.cardNumberField.frame = CGRectMake(self.frame.size.width - 20.0 - 183.0,
+                                            self.hundredKeysQuantity.frame.origin.y + self.hundredKeysQuantity.frame.size.height + 20.0,
+                                           183.0,
+                                           35.0);
+    self.cardNumberLabel.frame = CGRectMake(20.0,
+                                                self.cardNumberField.frame.origin.y + (self.cardNumberField.frame.size.height - self.cardNumberLabel.frame.size.height) / 2,
+                                                self.cardNumberLabel.frame.size.width,
+                                                textSize.height);
+    
+    textSize = [self.expirationDateLabel.text boundingRectWithSize:CGSizeMake(self.expirationDateLabel.frame.size.width, MAXFLOAT)
+                                                       options:NSStringDrawingUsesLineFragmentOrigin
+                                                    attributes:@{NSFontAttributeName:self.expirationDateLabel.font}
+                                                       context:nil].size;
+    self.expirationDateField.frame = CGRectMake(self.frame.size.width - 20.0 - 98.0,
+                                            self.cardNumberField.frame.origin.y + self.cardNumberField.frame.size.height + 8.0,
+                                            98.0,
+                                            35.0);
+    self.expirationDateLabel.frame = CGRectMake(20.0,
+                                            self.expirationDateField.frame.origin.y + (self.expirationDateField.frame.size.height - self.expirationDateLabel.frame.size.height) / 2,
+                                            self.expirationDateLabel.frame.size.width,
+                                            textSize.height);
+    
+    textSize = [self.securityCodeLabel.text boundingRectWithSize:CGSizeMake(self.securityCodeLabel.frame.size.width, MAXFLOAT)
+                                                       options:NSStringDrawingUsesLineFragmentOrigin
+                                                    attributes:@{NSFontAttributeName:self.securityCodeLabel.font}
+                                                       context:nil].size;
+    self.securityCodeField.frame = CGRectMake(self.frame.size.width - 20.0 - 64.0,
+                                            self.expirationDateField.frame.origin.y + self.expirationDateField.frame.size.height + 8.0,
+                                            64.0,
+                                            35.0);
+    self.securityCodeLabel.frame = CGRectMake(20.0,
+                                            self.securityCodeField.frame.origin.y + (self.securityCodeField.frame.size.height - self.securityCodeLabel.frame.size.height) / 2,
+                                            self.securityCodeLabel.frame.size.width,
+                                            textSize.height);
+    
+    textSize = [self.zipCodeLabel.text boundingRectWithSize:CGSizeMake(self.zipCodeLabel.frame.size.width, MAXFLOAT)
+                                                       options:NSStringDrawingUsesLineFragmentOrigin
+                                                    attributes:@{NSFontAttributeName:self.zipCodeLabel.font}
+                                                       context:nil].size;
+    self.zipCodeField.frame = CGRectMake(self.frame.size.width - 20.0 - 102.0,
+                                            self.securityCodeField.frame.origin.y + self.securityCodeField.frame.size.height + 8.0,
+                                            102.0,
+                                            35.0);
+    self.zipCodeLabel.frame = CGRectMake(20.0,
+                                            self.zipCodeField.frame.origin.y + (self.zipCodeField.frame.size.height - self.zipCodeLabel.frame.size.height) / 2,
+                                            self.zipCodeLabel.frame.size.width,
+                                            textSize.height);
 }
 
 
 #pragma mark - Listeners
 
 - (void)tapKeyQuantity:(UIButton *)sender {
+    if (sender == self.buyTenKeys) {
+        self.selectedKeys = self.buyTenKeys;
+        self.buyTenKeys.layer.opacity = 1.0;
+        self.buyTenKeys.backgroundColor = [[Util sharedManager] colorWithHexString:[Util getBlueColorHex]];
+        self.buyFiftyKeys.layer.opacity = 0.33;
+        self.buyFiftyKeys.backgroundColor = [UIColor clearColor];
+        self.buyHundredKeys.layer.opacity = 0.33;
+        self.buyHundredKeys.backgroundColor = [UIColor clearColor];
+    } else if (sender == self.buyFiftyKeys) {
+        self.selectedKeys = self.buyFiftyKeys;
+        self.buyTenKeys.layer.opacity = 0.33;
+        self.buyTenKeys.backgroundColor = [UIColor clearColor];
+        self.buyFiftyKeys.layer.opacity = 1.0;
+        self.buyFiftyKeys.backgroundColor = [[Util sharedManager] colorWithHexString:[Util getBlueColorHex]];
+        self.buyHundredKeys.layer.opacity = 0.33;
+        self.buyHundredKeys.backgroundColor = [UIColor clearColor];
+    } else if (sender == self.buyHundredKeys) {
+        self.selectedKeys = self.buyHundredKeys;
+        self.buyTenKeys.layer.opacity = 0.33;
+        self.buyTenKeys.backgroundColor = [UIColor clearColor];
+        self.buyFiftyKeys.layer.opacity = 0.33;
+        self.buyFiftyKeys.backgroundColor = [UIColor clearColor];
+        self.buyHundredKeys.layer.opacity = 1.0;
+        self.buyHundredKeys.backgroundColor = [[Util sharedManager] colorWithHexString:[Util getBlueColorHex]];
+    }
 }
 
 - (void)tapBuy:(UIButton *)sender {
+    
 }
 
 /*
