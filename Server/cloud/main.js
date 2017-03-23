ce46e78b1be14168de1897117103bee1ea8b7ec6
William Huang <whuang6@stanford.edu>
2017-03-23 02:08:33 -0700

    remove cloud code

--- Server/cloud/main.js
+++ Server/cloud/main.js
@@ -2,32 +2,3 @@
 Parse.Cloud.define('hello', function(req, res) {
   res.success('Hi');
 });
-
-Parse.Cloud.define("addCredits", function(request, response) {
-	Parse.Cloud.useMasterKey();
-	var objectId = request.params.objectId;
-	var credits = request.params.credits;
-	var query = new Parse.Query(Parse.User);
-	query.equalTo("objectId", objectId);
-	query.include("credits");
-	query.first({
-	    success: function(userRecord) {
-	    	userRecord.fetch();
-			userRecord.set("credits", userRecord.get("credits") + credits);
-			userRecord.save({useMasterKey: true}, {
-                success: function(user) {
-                    console.log('save user success');
-                    response.success("save user success")
-                    userRecord.fetch();
-                },
-                error: function(error) {
-                    console.log("failed to save user");
-                    response.error(error);
-                }
-            });
-	    },
-	    error: function(error) {
-	      response.error("add credits failed");
-	    }
-	});
-});
